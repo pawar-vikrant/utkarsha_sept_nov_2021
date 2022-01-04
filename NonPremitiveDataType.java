718059ebe1c74738b98c1b6b661a031a47f63caf
pawar-vikrant <97036776+pawar-vikrant@users.noreply.github.com>
2022-01-04 20:28:34 +0530

    data
    javaCode

---
+++ NonPremitiveDataType.java
@@ -0,0 +1,23 @@
+class NonpermitiveDataType
+{public static void main(String args []){
+String str = 'test';
+System.out.println("String is : " + str);
+String str1 = new String('test')
+System.out.println("another String is " + str1);
+
+int arr[];
+arr = new int [2];
+arr [0] = 0;
+arr [1] = 1;
+//arr [2] = 2;
+
+System.out.println(arr);
+System.out.println("0"); 
+
+}
+
+
+
+
+
+}
